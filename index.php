5ca61de7cdf94acd86cb42581bad16c5b3d3ef2a
victordf <victormachado90@gmail.com>
2015-08-18 07:55:09 -0300

    Início do projeto
    Iniciação do projeto

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: VictorPC
+ * Date: 18/08/2015
+ * Time: 07:43
+ */
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Portal Asa Norte</title>
+
+        <link rel="stylesheet" href="css/bootstrap.css"/>
+
+        <script src="js/angular.js"></script>
+        <script src="js/angular-messages.js"></script>
+        <script src="js/bootstrap.js"></script>
+    </head>
+
+    <body>
+
+    </body>
+</html>
